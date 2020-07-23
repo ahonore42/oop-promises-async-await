f5a439fea2b0d882987100fbd7ff250af4618fe6
ahonore42 <a1honore80@gmail.com>
2020-07-23 17:56:33 -0400

    add constructor functions

--- app.js
+++ app.js
@@ -41,3 +41,38 @@ console.log(apartTwo.rooms);
 console.log(apartOne.rooms);
 
 // Constructor Functions
+function User(name) {
+    // this
+    this.name = name;
+
+    // return this
+}
+
+let adam = new User('Adam');
+let pete = new User('Pete')
+console.log(adam.name)
+console.log(pete.name)
+
+function NBAPLayer(name, team, threePointer) {
+    this.name = name;
+    this.team = team;
+    this.threePointer = threePointer;
+
+    // return this
+}
+
+let steph = new NBAPLayer('Steph Curry', 'Warriors', true)
+
+// make a constructor function with 3 new variables
+
+function RememberTheName(luck, skill, powerOfWill, pleasure, pain, remember) {
+    this.luck = luck
+    this.skill = skill
+    this.powerOfWill = powerOfWill
+    this.pleasure = pleasure
+    this.pain = pain
+    this.remember = remember
+}
+
+let yaBoi = new RememberTheName('10%', '20%', '15%', '5%', '50%', true)
+console.log(yaBoi)
